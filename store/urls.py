e57294ef62f2c27f15555e99f3972789b7934645
Liam B <lpb7@njit.edu>
2024-04-22 11:48:51 -0400

    added readme and gitignore

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns  = [
-    path('', views.index, name='index'),
-    path('menu/', views.menu, name='menu'),
-    path('checkout/', views.checkout, name='checkout'),
-    path('thankyou/', views.thankyou, name='thankyou'),
-]
